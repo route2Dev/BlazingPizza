64356b02c245ace1ca2dc21760dc24d777ea2770
Dave Baumann <dave.baumann@route2solutions.com>
2025-02-16 02:39:57 -0600

    access data from a Blazor component

---
+++ Controllers/SpecialsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Data;
+
+namespace BlazingPizza.Controllers;
+
+[Route("specials")]
+[ApiController]
+public class SpecialsController(PizzaStoreContext db) : Controller
+{
+    private readonly PizzaStoreContext _db = db;
+
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using BlazingPizza.Components;
+using BlazingPizza.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -18,11 +23,23 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
