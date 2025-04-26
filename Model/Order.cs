79259d0a1f1d520e30479f81ebb6223f70cdc505
Dave Baumann <dave.baumann@route2solutions.com>
2025-04-26 14:59:43 -0500

    add entity framework support for orders and pizzas

--- Model/Order.cs
+++ Model/Order.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace BlazingPizza
+﻿namespace BlazingPizza
 {
     public class Order
     {
         public int OrderId { get; set; }
 
-        public string UserId { get; set; }
+        public string UserId { get; set; } = string.Empty;
 
         public DateTime CreatedTime { get; set; }
 
-        public Address DeliveryAddress { get; set; } = new Address();
+        //public Address DeliveryAddress { get; set; } = new Address();
 
-        public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+        public List<Pizza> Pizzas { get; set; } = [];
 
         public decimal GetTotalPrice() => Pizzas.Sum(p => p.GetTotalPrice());
 
